75448d3236ef64dbbaf5d51442bd695bc791be7a
ScuolaBInf <cinquinidavide1996@gmail.com>
2017-02-21 02:34:40 +0100

    .

--- avvia.cpp
+++ avvia.cpp
@@ -3,20 +3,26 @@
 using namespace std;
 
 void inserisciTempo(int tempo);
+void Inizializza();
 
 main()
 	{
 		int tempo = 0;
-		FILE* fp = fopen("tempo.txt", "w");
-		fprintf(fp, "%d", 0);		
-		fclose(fp);	
-		system("start gioco.exe");
+		Inizializza();
 		while(true)
 			{
 				inserisciTempo(tempo);
 				Sleep(37);
 				tempo+=37;
 			}
+	}
+
+void Inizializza()
+	{
+		FILE* fp = fopen("tempo.txt", "w");
+		fprintf(fp, "%d", 0);		
+		fclose(fp);	
+		system("start gioco.exe");		
 	}
 
 void inserisciTempo(int tempo)
